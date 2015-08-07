068b16732197a3deb70f98d58176d54b222b2aa9
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-07 18:23:38 +0300

    lesson2_task1

--- js_main/lesson2/task1/js.js
+++ js_main/lesson2/task1/js.js
@@ -2,13 +2,14 @@
  * Created by ProFi on 07.08.2015.
  */
 
-
+//defining function yearAnswer
 function yearAnswer() {
+    //defining variable year
     var year = parseInt(document.getElementById("year").value);
+    //conditional statements
     if (year === 2015) {
         document.getElementById('result').value = "Vy pravy!";
     } else {
         document.getElementById('result').value = "S luny svalilsya? 2015!";
     }
 }
-console.log(typeof(year));
