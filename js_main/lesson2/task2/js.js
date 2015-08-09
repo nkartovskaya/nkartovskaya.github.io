14a533ce29f41e3fcd3e0ad221594e0814c5cb57
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-09 11:15:55 +0300

    lesson2_task2

--- js_main/lesson2/task2/js.js
+++ js_main/lesson2/task2/js.js
@@ -1,3 +1,17 @@
-/**
- * Created by ProFi on 07.08.2015.
- */
+//defining function yearResult
+function yourResult() {
+    //defining variables
+    var result = parseInt(document.getElementById('yourNumber').value);
+    //checking number >0
+    if (result > 0) {
+        //display data if number>0
+        document.getElementById('yourResult').value = 1;
+        //checking number <0
+    } else if (result < 0) {
+        //display data if number<0
+        document.getElementById('yourResult').value = -1;
+    } else {
+        //display data if number=0
+        document.getElementById('yourResult').value = 0;
+    }
+}
