9c2b3c568e5448b8d9bb7610d3fe2186dddb3ab1
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-09 12:14:31 +0300

    lesson2_task5

---
+++ js_main/lesson2/task5/js.js
@@ -0,0 +1,4 @@
+//defining variables
+var name = 'admin', text;
+//checking name
+text = (name == 'admin') ? text = 'Hi' : (name == 'manager') ? text = 'Hello' : (name == '') ? text = 'No login' : text = '';
