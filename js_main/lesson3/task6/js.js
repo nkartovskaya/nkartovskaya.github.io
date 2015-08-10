36cdae00a860c5fe823c4d16e0176fe4d5b8163c
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-10 21:39:21 +0300

    lesson3_task6

--- js_main/lesson3/task6/js.js
+++ js_main/lesson3/task6/js.js
@@ -1,5 +1,15 @@
-function pow(x,n) {
-    for (i=1; i<=n; i++) {
-
+//self-invoking function for calculating pow n number x
+(function (x, n) {
+    //defining variables
+    var powX = x;
+    //loop for multiplication x*x n times
+    for (i = 2; i <= n; i++) {
+        //multiplication
+        powX = powX * x;
     }
-}
+    //display data
+    console.log(powX);
+    //function arguments
+})(5, 2);
+
+
