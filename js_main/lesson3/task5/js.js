afcecff850a899bf3428c0b034c9d9bfe68f83e7
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-10 21:11:07 +0300

    lesson3_task5

--- js_main/lesson3/task5/js.js
+++ js_main/lesson3/task5/js.js
@@ -1,26 +1,26 @@
 /*Напишите программу, создающую строку, содержащую решётку 8х8,
-в которой линии разделяются символами новой строки.
-На каждой позиции либо пробел, либо #. В результате должна получиться
-шахматная доска.
+ в которой линии разделяются символами новой строки.
+ На каждой позиции либо пробел, либо #. В результате должна получиться
+ шахматная доска.
 
  # # # #
-  # # # #
  # # # #
-  # # # #
  # # # #
-  # # # #
  # # # #
-  # # # #
+ # # # #
+ # # # #
+ # # # #
+ # # # #
  */
 
 //loop for lines
-    for (j=0; j<8; j++) {
-        //checking if j is even number
-        if (j%2==0) {
-            //display line if j is even number
-            console.log('# # # #');
-        } else {
-            //display data if j is odd number
-            console.log(' # # # #');
-        }
+for (j = 0; j < 8; j++) {
+    //checking if j is even number
+    if (j % 2 == 0) {
+        //display line if j is even number
+        console.log('# # # #');
+    } else {
+        //display data if j is odd number
+        console.log(' # # # #');
     }
+}
---
+++ js_main/lesson3/task6/js.js
@@ -0,0 +1,5 @@
+function pow(x,n) {
+    for (i=1; i<=n; i++) {
+
+    }
+}
