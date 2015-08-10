8f41baf84a76c27e103a1b8afbff677599aaf227
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-10 21:11:27 +0300

    lesson3_task4

--- js_main/lesson3/task4/js.js
+++ js_main/lesson3/task4/js.js
@@ -1,11 +1,11 @@
 /*Исправьте 1ю задачу так, чтобы она выводила «FizzBuzz»
-для всех чисел, которые делятся и на 3, и на 5.
+ для всех чисел, которые делятся и на 3, и на 5.
  */
 
 //loop for number that is checked
-for (i=1; i<=100; i++) {
+for (i = 1; i <= 100; i++) {
     //checking if i%5==0 and i%3==0
-    if (i%5==0&&i%3==0) {
+    if (i % 5 == 0 && i % 3 == 0) {
         //display data
         console.log('FizzBuzz');
         //all number, if first and condition is wrong
