bc20de769bf4cfeb485771ab6db4423ba90db330
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-10 21:12:04 +0300

    lesson3_task2

--- js_main/lesson3/task2/js.js
+++ js_main/lesson3/task2/js.js
@@ -1,5 +1,5 @@
 /*Ќатуральное число, большее 1, называетс€ простым, если оно ни на что не делитс€,
-кроме себ€ и 1.
+ кроме себ€ и 1.
  ƒругими словами, n>1 Ц простое, если при делении на любое число от 2 до n-1
  есть остаток.
  —оздайте код, который выводит все простые числа из интервала от 2 до 10.
@@ -7,12 +7,12 @@
  P.S.  од также должен легко модифицироватьс€ дл€ любых других интервалов.*/
 
 //loop for number, that is checked. Loop starts from 2, because 1 is not prime number
-for (var i=2; i<=10; i++){
+for (var i = 2; i <= 10; i++) {
     //loop number-denominator
-    for(var j=2; j<=i; j++){
+    for (var j = 2; j <= i; j++) {
         //checking remainder of division, if it==0, loop breaks
-        if (i%j == 0) break;
+        if (i % j == 0) break;
     }
     //checking number==denominator, if ==, this number is prime
-    if(j==i) console.log(i);
+    if (j == i) console.log(i);
 }
