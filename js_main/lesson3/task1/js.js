7585b834045382364a10588d7309b6a17379570c
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-12 16:27:48 +0300

    lesson3_task1

--- js_main/lesson3/task1/js.js
+++ js_main/lesson3/task1/js.js
@@ -10,11 +10,11 @@
 
 
 //loop 'Vvedite chislo bolshe 0'
-for (i = 0; ; i++) {
+for (var i = 0; ; i++) {
     //defining variables
-    var number = prompt('Vvedite chislo bolshe 100', "");
+    var number = +prompt('Vvedite chislo bolshe 100', "");
     //checking number >100 or cancel
-    if (+number > 100 || number == null) {
+    if (number > 100 || number == 0) {
         //display result >100 or cancel
         alert('Finish');
         break;
