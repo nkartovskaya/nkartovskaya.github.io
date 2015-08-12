9d4a3875934795197932798cd5eac9b8f85b074d
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-12 15:29:14 +0300

    lesson3_task5

--- js_main/lesson3/task5/js.js
+++ js_main/lesson3/task5/js.js
@@ -4,17 +4,17 @@
  шахматная доска.
 
  # # # #
+  # # # #
  # # # #
+  # # # #
  # # # #
+  # # # #
  # # # #
- # # # #
- # # # #
- # # # #
- # # # #
+  # # # #
  */
 
 //loop for lines
-for (j = 0; j < 8; j++) {
+for (var j = 0; j < 8; j++) {
     //checking if j is even number
     if (j % 2 == 0) {
         //display line if j is even number
