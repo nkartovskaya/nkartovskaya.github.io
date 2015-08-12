43159766fefa7fbfcfed803269cf8f70744180d2
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-12 15:24:14 +0300

    lesson3_task1

--- js_main/lesson3/task1/js.js
+++ js_main/lesson3/task1/js.js
@@ -1,9 +1,20 @@
+/*Напишите цикл, который предлагает prompt ввести число, большее 100.
+Если посетитель ввел другое число — попросить ввести еще раз, и так далее.
+
+ Цикл должен спрашивать число пока либо посетитель не введет число, большее
+ 100, либо не нажмет кнопку Cancel (ESC).
+
+ Предполагается, что посетитель вводит только числа, предусматривать
+ обработку нечисловых строк в этой задаче необязательно.
+ */
+
+
 //loop 'Vvedite chislo bolshe 0'
 for (i = 0; ; i++) {
     //defining variables
     var number = prompt('Vvedite chislo bolshe 100', "");
     //checking number >100 or cancel
-    if (number > 100 || number == null) {
+    if (+number > 100 || number == null) {
         //display result >100 or cancel
         alert('Finish');
         break;
