b0f9661ee243ead39c55c12809acae46b077adb8
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-12 17:01:41 +0300

    lesson3_task8

--- js_main/lesson3/task8/js.js
+++ js_main/lesson3/task8/js.js
@@ -3,4 +3,16 @@ However, she's in love with Johnny, and would like to greet him
 slightly different. She added a special case to her function, but she made a mistake.
  Can you help her?*/
 
-
+//function for greeting
+function greet(name){
+    //checking Johnny
+    if(name === "Johnny") {
+        //return for Johnny
+        return "Hello, my love!";
+    } else {
+        //return for other people
+        return "Hello, " + name + "!";
+    }
+}
+//display data
+console.log(greet("Johnny"));
