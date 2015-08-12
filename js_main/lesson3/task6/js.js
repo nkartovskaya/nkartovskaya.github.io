07b7c4e81bcaef1267f70c37b0ef23bd82320814
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-12 15:30:46 +0300

    lesson3_task6

--- js_main/lesson3/task6/js.js
+++ js_main/lesson3/task6/js.js
@@ -1,9 +1,14 @@
-//self-invoking function for calculating pow n number x
+/*
+ Напишите функцию pow(x,n), которая возвращает x в степени n.
+ Иначе говоря, умножает x на себя n раз и возвращает результат.
+ */
+
+//function for calculating pow n number x
 (function (x, n) {
     //defining variables
     var powX = x;
     //loop for multiplication x*x n times
-    for (i = 2; i <= n; i++) {
+    for (var i = 2; i <= n; i++) {
         //multiplication
         powX = powX * x;
     }
