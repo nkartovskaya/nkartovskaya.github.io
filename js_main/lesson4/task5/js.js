af8d451e39a644a87790384fa6397ab19efc4aeb
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-17 10:46:29 +0300

    task 22

--- js_main/lesson4/task5/js.js
+++ js_main/lesson4/task5/js.js
@@ -1,3 +1,13 @@
-/**
- * Created by ProFi on 14.08.2015.
- */
+/*рассчитать сумму членов арифметической програссии от 1 до n с шагом d
+ члены арифметической прогрессии a[n]=a[1]+(n-1)*d
+ сумма членов арифметической прогрессии s=(a[1]+a[n])/2*n
+
+ * */
+
+(function (n, d) {
+    var aN, s;
+    aN = 1 + (n - 1) * d;
+    s = (1 + aN) / 2 * n;
+    console.log("Chlen arifmeticheskoj progressii n=" + aN);
+    console.log("Summa chlenov arifmeticheskoj progressii ot 1 do n=" + s);
+})(10, 2);
