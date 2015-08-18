acdba3b3108d5463683cdfa11fc02aa0528b0ecf
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-18 14:54:06 +0300

    update

--- js_main/lesson4/task1/js.js
+++ js_main/lesson4/task1/js.js
@@ -10,25 +10,49 @@
  console.log(fib(77)); //5527939700884757
  */
 
-//function for calculation Fibonacci number
-function fib(number) {
-    //defining variable, new Array for Fibonacci number
-    var fibLine = new Array(number);
-    //assigning value to first item of Fibonacci numbers
-    fibLine[0] = 1;
-    //assigning value to second item of Fibonacci number
-    fibLine[1] = 1;
-    //loop for calculation of Fibonacci numbers from third item
-    for (var i = 2; i < number; i++) {
-        //calculation of Fibonacci numbers from third item
-        fibLine[i] = fibLine[i - 1] + fibLine[i - 2];
+
+/*
+ //function for calculation Fibonacci number
+ function fib(number) {
+ //defining variable, new Array for Fibonacci number
+ var fibLine = new Array(number);
+ //assigning value to first item of Fibonacci numbers
+ fibLine[0] = 1;
+ //assigning value to second item of Fibonacci number
+ fibLine[1] = 1;
+ //loop for calculation of Fibonacci numbers from third item
+ for (var i = 2; i < number; i++) {
+ //calculation of Fibonacci numbers from third item
+ fibLine[i] = fibLine[i - 1] + fibLine[i - 2];
+ }
+ //display Fibonacci numbers
+ console.log(fibLine);
+ //display Fibonacci number on place(number)
+ console.log(fibLine[number - 1]);
+ }
+ //function call
+ fib(15);
+ */
+
+//functional for calculation Fibonacci number
+function fibNumber(number) {
+    //defining variables-Fibonacci number
+    var fib;
+//condition for number >2, because fib1 and fib2 == 1
+    if (number >= 2) {
+        //calculation Fibonacci number, calling function for number-1 and number-2
+        fib = fibNumber(number - 1) + fibNumber(number - 2);
+        //if number<2 Fibonacci number ==1 (fib1, fib2)
+    } else {
+        //fib number for number<2
+        fib = number;
     }
-    //display Fibonacci numbers
-    console.log(fibLine);
-    //display Fibonacci number on place(number)
-    console.log(fibLine[number - 1]);
+    //returning result
+    return fib;
 }
-//function call
-fib(15);
+//dispaly data for number=15
+console.log(fibNumber(15));
+
+
 
 
