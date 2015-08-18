e59ce9b96ac3fedf625c5ed5f8719fde02ff14d9
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-18 15:21:15 +0300

    update

--- js_main/lesson4/task5/js.js
+++ js_main/lesson4/task5/js.js
@@ -4,10 +4,17 @@
 
  * */
 
+//function for calculation sum of n items with step=d
 (function (n, d) {
+    //defining variables: aN - item, s - sum
     var aN, s;
+    //calculation item with number=n, if step=d
     aN = 1 + (n - 1) * d;
+    //calculation sum
     s = (1 + aN) / 2 * n;
+    //display item with number n
     console.log("Chlen arifmeticheskoj progressii n=" + aN);
+    //display sum
     console.log("Summa chlenov arifmeticheskoj progressii ot 1 do n=" + s);
+    //data: number and step
 })(10, 2);
