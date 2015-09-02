06e5d35a8756360612b1fddc996f6647a9e554fb
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-09-02 18:35:25 +0300

    lesson8_task4

--- js_main/lesson8/task4/js.js
+++ js_main/lesson8/task4/js.js
@@ -23,6 +23,29 @@
  Article.showStats(); // Всего: 3, Последняя: (дата)
  */
 
+//creating function for calculating objects and saving last's object date
 function Article() {
-
+    //increasing count
+    Article.count++;
+    //creating date
+    Article.created = new Date();
 }
+//adding count and assigning it's value
+Article.count = 0;
+//creating method for showing of amount of objects and last object's date
+Article.showStats = function () {
+    //display result
+    console.log("Vsego: " + this.count + " Posledniaya data: " + Article.created);
+}
+//creating new object
+new Article();
+//calling method showStats
+Article.showStats();
+//creating new object
+new Article();
+//calling method showStats
+Article.showStats();
+//creating new object
+new Article();
+//calling method showStats
+Article.showStats();
