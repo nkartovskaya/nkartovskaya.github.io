d56178dc90db95f69284f5f895a7475962bec36e
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-09-15 16:07:06 +0300

    traffic_light

--- js_vacation/traffic_light/js.js
+++ js_vacation/traffic_light/js.js
@@ -4,27 +4,117 @@
  настоящий (в плане моргания).
  */
 
-function normalWork() {
-    setInterval(function () {
-        setTimeout(onRed, 500);
-        setTimeout(onYellow, 1500);
-        setTimeout(onGreen, 2500);
-    }, 3000);
-}
-function onRed() {
-    document.getElementById("redLight").style.backgroundColor = "red";
-    document.getElementById("yellowLight").style.backgroundColor = "grey";
-    document.getElementById("greenLight").style.backgroundColor = "grey";
-}
-function onYellow() {
-    document.getElementById("redLight").style.backgroundColor = "grey";
-    document.getElementById("yellowLight").style.backgroundColor = "yellow";
-    document.getElementById("greenLight").style.backgroundColor = "grey";
-}
-function onGreen() {
-    document.getElementById("redLight").style.backgroundColor = "grey";
-    document.getElementById("yellowLight").style.backgroundColor = "grey";
-    document.getElementById("greenLight").style.backgroundColor = "green";
+
+function getElStyleBgC(elId, color) {
+    return document.getElementById(elId).style.backgroundColor = color;
 }
 
+function normalWork() {
+    setInterval(function () {
+        setTimeout(onRed, 1);
+        setTimeout(flashingRed, 2000);
+        setTimeout(onYellow, 5000);
+        setTimeout(flashingYellow, 8000);
+        setTimeout(onGreen, 11000);
+        setTimeout(flashingGreen, 14000);
+    }, 18000);
+}
+
+function onRed() {
+    getElStyleBgC("redLight", "red");
+    getElStyleBgC("yellowLight", "grey");
+    getElStyleBgC("greenLight", "grey");
+}
+
+function onYellow() {
+    getElStyleBgC("redLight", "grey");
+    getElStyleBgC("yellowLight", "yellow");
+    getElStyleBgC("greenLight", "grey")
+}
+
+function onGreen() {
+    getElStyleBgC("redLight", "grey");
+    getElStyleBgC("yellowLight", "grey");
+    getElStyleBgC("greenLight", "green");
+}
+
+var speed = 500;
+var temp = 1;
+var countRed=0;
+function flashingRed() {
+    var flash = setTimeout("flashingRed()", speed);
+    if (temp == 1) {
+        getElStyleBgC("redLight", "grey");
+        temp = 0;
+        countRed++;
+        console.log(countRed);
+        if (countRed >= 6) {
+            clearTimeout(flash);
+            countRed=0;
+        }
+    }
+    else {
+        getElStyleBgC("redLight", "red");
+        temp = 1;
+        countRed++;
+        console.log(countRed);
+        if (countRed >= 6) {
+            clearTimeout(flash);
+            countRed=0;
+        }
+    }
+    flash;
+}
+
+var countYellow=0;
+function flashingYellow() {
+    var flash = setTimeout("flashingYellow()", speed);
+    if (temp == 1) {
+        getElStyleBgC("yellowLight", "grey");
+        temp = 0;
+        countYellow++;
+        console.log(countYellow);
+        if (countYellow >= 6) {
+            clearTimeout(flash);
+            countYellow=0;
+        }
+    }
+    else {
+        getElStyleBgC("yellowLight", "yellow");
+        temp = 1;
+        countYellow++;
+        console.log(countYellow);
+        if (countYellow >= 6) {
+            clearTimeout(flash);
+            countYellow=0;
+        }
+    }
+    flash;
+}
+
+var countGreen=0;
+function flashingGreen() {
+    var flash = setTimeout("flashingGreen()", speed);
+    if (temp == 1) {
+        getElStyleBgC("greenLight", "grey");
+        temp = 0;
+        countGreen++;
+        console.log(countGreen);
+        if (countGreen >= 6) {
+            clearTimeout(flash);
+            countGreen=0;
+        }
+    }
+    else {
+        getElStyleBgC("greenLight", "green");
+        temp = 1;
+        countGreen++;
+        console.log(countGreen);
+        if (countGreen >= 6) {
+            clearTimeout(flash);
+            countGreen=0;
+        }
+    }
+    flash;
+}
 
---
+++ js_vacation/traffic_light_object/js.js
@@ -0,0 +1,152 @@
+/**
+ Автомобильный светофор (3 цвета). Сделать возможность переключение автоматически
+ и по нажатию на клавишу. При переключении светофор должен вести себя так как
+ настоящий (в плане моргания).
+ */
+
+
+var container=document.createElement("div");
+container.id="trafficLight";
+container.style.width="100px";
+container.style.height="300px";
+container.style.backgroundColor="black";
+container.style.padding="5px";
+
+var newDiv = document.createElement('div')
+newDiv.className = 'my-class'
+newDiv.id = 'my-id'
+newDiv.style.backgroundColor = 'red'
+newDiv.innerHTML = 'Привет, мир!'
+
+
+/*var trafficLight={
+    createTrafficLight: function() {
+      var container=document.createElement("div");
+        container.id="trafficLight";
+        container.style.width="100px";
+        container.style.height="300px";
+        container.style.backgroundColor="black";
+        container.style.padding="5px";
+
+        var redLight=container.createElement("div");
+        redLight.innerHTML
+        container.createElement("div");
+        container.createElement("div");
+    },
+    normalWork: function() {
+        setInterval(function () {
+            setTimeout(onRed, 1);
+            setTimeout(flashingRed, 2000);
+            setTimeout(onYellow, 5000);
+            setTimeout(flashingYellow, 8000);
+            setTimeout(onGreen, 11000);
+            setTimeout(flashingGreen, 14000);
+        }, 18000);
+    },
+    onRed: function() {
+        getElStyleBgC("redLight", "red");
+        getElStyleBgC("yellowLight", "grey");
+        getElStyleBgC("greenLight", "grey");
+    },
+    onYellow: function() {
+        getElStyleBgC("redLight", "grey");
+        getElStyleBgC("yellowLight", "yellow");
+        getElStyleBgC("greenLight", "grey")
+    },
+    onGreen: function() {
+        getElStyleBgC("redLight", "grey");
+        getElStyleBgC("yellowLight", "grey");
+        getElStyleBgC("greenLight", "green");
+    },
+    flashingRed: function() {
+        var flash = setTimeout("flashingRed()", speed);
+        if (temp == 1) {
+            getElStyleBgC("redLight", "grey");
+            temp = 0;
+            countRed++;
+            console.log(countRed);
+            if (countRed >= 6) {
+                clearTimeout(flash);
+                countRed=0;
+            }
+        }
+        else {
+            getElStyleBgC("redLight", "red");
+            temp = 1;
+            countRed++;
+            console.log(countRed);
+            if (countRed >= 6) {
+                clearTimeout(flash);
+                countRed=0;
+            }
+        }
+        flash;
+    },
+    flashingYellow: function() {
+        var flash = setTimeout("flashingYellow()", speed);
+        if (temp == 1) {
+            getElStyleBgC("yellowLight", "grey");
+            temp = 0;
+            countYellow++;
+            console.log(countYellow);
+            if (countYellow >= 6) {
+                clearTimeout(flash);
+                countYellow=0;
+            }
+        }
+        else {
+            getElStyleBgC("yellowLight", "yellow");
+            temp = 1;
+            countYellow++;
+            console.log(countYellow);
+            if (countYellow >= 6) {
+                clearTimeout(flash);
+                countYellow=0;
+            }
+        }
+        flash;
+    },
+    flashingGreen: function() {
+        var flash = setTimeout("flashingGreen()", speed);
+        if (temp == 1) {
+            getElStyleBgC("greenLight", "grey");
+            temp = 0;
+            countGreen++;
+            console.log(countGreen);
+            if (countGreen >= 6) {
+                clearTimeout(flash);
+                countGreen=0;
+            }
+        }
+        else {
+            getElStyleBgC("greenLight", "green");
+            temp = 1;
+            countGreen++;
+            console.log(countGreen);
+            if (countGreen >= 6) {
+                clearTimeout(flash);
+                countGreen=0;
+            }
+        }
+        flash;
+    }
+}
+
+
+function getElStyleBgC(elId, color) {
+    return document.getElementById(elId).style.backgroundColor = color;
+}
+
+
+var speed = 500;
+var temp = 1;
+var countRed=0;
+
+
+var countYellow=0;
+
+
+var countGreen=0;
+*/
+
+
