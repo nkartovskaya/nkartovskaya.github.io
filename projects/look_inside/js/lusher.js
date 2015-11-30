9a960973c975a1b556114bc8f07428548395764d
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-11-30 18:42:45 +0200

    update

--- projects/look_inside/js/lusher.js
+++ projects/look_inside/js/lusher.js
@@ -1,3 +1,8 @@
-/**
- * Created by ProFi on 29.11.2015.
- */
+var colors=["blue","green","yellow","black","brown","red","grey","violet"];
+var newBlock=document.createElement("div");
+var parentBlock=document.getElementById('content');
+function firstCase() {
+	newBlock.style.backgroundColor=colors[1];
+	parentBlock.appendChild(newBlock);
+
+}
